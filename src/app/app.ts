b5d22859b2756d577f5e0efe3f1c85aec1e2d9ce
Alan Alvarez <aran.712@gmail.com>
2026-05-19 19:01:38 -0600

    header was created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './ui/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
