47b9c852ab7d80ba87e1f830478ba27739da0d3d
Alan Alvarez <aran.712@gmail.com>
2026-05-19 19:36:00 -0600

    router was added between home and movie page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './ui/layout/header/header.component';
 import { FooterComponent } from "./ui/layout/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
